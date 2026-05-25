665191d446b5307ce354881f6f98f9095962aa54
centurytradingcompanyltd-hue <centurytradingcompanyltd@gmail.com>
2026-05-25 08:07:52 +0300

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -23,7 +23,6 @@
       mainClass: 'mfp-fade',
       removalDelay: 160,
       preloader: false,
-
       fixedContentPos: false
     });
 
@@ -61,14 +60,9 @@
       autoplay:true,
       smartSpeed:1500,
       responsive:{
-        0:{
-            items:1
-        },
-
-        992:{
-            items:2
-        }
-    }
+        0:{ items:1 },
+        992:{ items:2 }
+      }
     });
 
     //Testimonial Slider Two
@@ -99,91 +93,33 @@
       smartSpeed:2000,
       autoplayHoverPause:true,
       responsive:{
-        0:{
-            items:1
-        },
-        600: {
-          items:2
-        },
-
-        768:{
-            items:3
-        },
-
-        1000: {
-          items:5
-        }
-    }
+        0:{ items:1 },
+        600:{ items:2 },
+        768:{ items:3 },
+        1000:{ items:5 }
+      }
     });
     
-    //Accordian JS
+    //Accordion JS
     $(".accordion-title").click(function(e){
         var accordionitem = $(this).attr("data-tab");
         $("#"+accordionitem).slideToggle().parent().siblings().find(".accordion-content").slideUp();
-
         $(this).toggleClass("active-title");
         $("#"+accordionitem).parent().siblings().find(".accordion-title").removeClass("active-title");
     });
 
+    //Scroll Top Button
     $(window).scroll(function () {
       if ($(this).scrollTop() != 0) {
           $('.top-btn').fadeIn();
-        }
-        else {
+      } else {
           $('.top-btn').fadeOut();
-        }
+      }
     });
 
     $('.top-btn').on('click',function(){
       $("html, body").animate({ scrollTop: 0 }, 1500);
       return false;
-    });
-
-    // Subscribe form
-    $(".newsletter-form").validator().on("submit", function (event) {
-      if (event.isDefaultPrevented()) {
-      // handle the invalid form...
-          formErrorSub();
-          submitMSGSub(false, "Please enter your email correctly.");
-      } else {
-          // everything looks good!
-          event.preventDefault();
-      }
-    });
-    function callbackFunction (resp) {
-      if (resp.result === "success") {
-          formSuccessSub();
-      }
-      else {
-          formErrorSub();
-      }
-    }
-    function formSuccessSub(){
-      $(".newsletter-form")[0].reset();
-      submitMSGSub(true, "Thank you for subscribing!");
-      setTimeout(function() {
-          $("#validator-newsletter").addClass('hide');
-      }, 4000)
-    }
-    function formErrorSub(){
-      $(".newsletter-form").addClass("animated shake");
-      setTimeout(function() {
-          $(".newsletter-form").removeClass("animated shake");
-      }, 1000)
-    }
-    function submitMSGSub(valid, msg){
-      if(valid){
-          var msgClasses = "validation-success";
-      } else {
-          var msgClasses = "validation-danger";
-      }
-      $("#validator-newsletter").removeClass().addClass(msgClasses).text(msg);
-    }
-
-    // AJAX MailChimp
-    $(".newsletter-form").ajaxChimp({
-      url: "https://envytheme.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
-      callback: callbackFunction
     });
 
     //WOW JS
@@ -197,6 +133,6 @@
     //Pre Loader
     $(window).on('load',function(){
       $(".loader-content").fadeOut(1000);
-    }) 
-    
+    });
+
 })(jQuery);
